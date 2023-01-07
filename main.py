f1da7c3b10e5850255b7e4c753b4fbd9e4c63e8b
Zanadukhan <cchiu231998@hotmail.ca>
2023-01-06 18:03:10 -0800

    amended gui so that only the resulting translation is shown

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import tkinter as tk
 import time
 import os
-auth_key = "insert api key here"
+auth_key = "insert auth key here"
 recent_value = ''
 
 deepl_api = Translator(auth_key)
@@ -33,48 +33,28 @@ paste.set(pyperclip.paste())
 translation = tk.StringVar()
 translation.set(translation_machine(pyperclip.paste()))
 
-root.geometry('700x300')
+root.geometry('700x500')
 
 root.title('Clipboard Translator')
 
-clipboard_title = tk.Label(root,
-                           text='Clipboard Translator',
-                           font=('Arial', 25),
-                           padx=20)
-
-clipboard_title.place(x=0, y=0)
-
-current_clipboard = tk.Label(root,
-                             textvariable=paste,
-                             font=('Arial', 15),
-                             bg='white',
-                             width=60,
-                             # height=2,
-                             anchor='nw',
-                             justify='left',
-                             padx=0,
-                             )
-current_clipboard.bind('<Configure>', lambda e: current_clipboard.config(wraplength=current_clipboard.winfo_width()))
-current_clipboard.place(x=20, y=50)
 
 translation_output = tk.Label(root,
                        text='Translated Output',
                        font=('Arial', 25),
                        padx=20)
-translation_output.place(x=0, y=140)
+translation_output.place(x=0, y=0)
 
 trans_output = tk.Label(root,
                         textvariable=translation,
                         font=('Arial', 15),
                         bg='white',
                         width=60,
-                        height=2,
                         anchor='nw',
                         justify='left',
                         padx=0,
                         )
 trans_output.bind('<Configure>', lambda e: trans_output.config(wraplength=trans_output.winfo_width()))
-trans_output.place(x=20, y=190)
+trans_output.place(x=20, y=50)
 
 
 run_listener(root, 500)
